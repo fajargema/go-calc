4269629362ad9f0a2f30acb88620d1ea6118505d
Fajar Gema <fgemar72@gmail.com>
2023-04-18 03:15:39 +0700

    deploy

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
 	fmt.Println(Add(2,3))
 	fmt.Println(Sum([]int{1, 2, 3, 4, 5}))
 	fmt.Println(Average([]int{1, 2, 3, 4, 5}))
+	e.Logger.Fatal(e.Start(":8080"))
+	
 }
 
 func Add(a int, b int) int {
